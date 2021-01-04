a4d9eb74cb172b243c0a358478a0058be8f75fdd
V-I-S-H-W-A <vis90sinha@gmail.com>
2021-01-04 12:04:30 +0530

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    [Route("[controller]")]
+    public class Class: ControllerBase
+    {
+        int a = 54;
+        int b = 89;
+        [HttpGet]
+        public int Get()
+        {
+            int x = a + b;
+            return x;
+        }
+        [HttpPut]
+        public int Put()
+        {
+            int x = a * b;
+            return x;
+        }
+        [HttpPost]
+        public int Post()
+        {
+            int x = b / a;
+            return x;
+        }
+        [HttpDelete]
+        public int Delete()
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
